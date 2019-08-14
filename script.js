bae948eb29c43625a4c402bcdb9af567f669c71e
leonardoInf <mondloslp@gmail.com>
2019-08-14 20:26:56 +0200

    first working version

--- script.js
+++ script.js
@@ -18,34 +18,46 @@ var rollHistory = [];                   //this is where previous rolls will be s
 var rollHistoryIndex = 1;               //holds the index of the currently viewed element
 
 $(document).ready(function(){          //when document is ready...
+
+    $(document).keydown(function(){    //capture keydowns
+        if(event.which == 74){ //j
+            if(! $("#previous").hasClass("disabled")) startPrevious();
+        }
+    
+        if(event.which == 75){ //k
+            if(! $("#roll-it").hasClass("disabled")) startRoll();    //allow keyinput if button is enabled
+        }
+        
+        if(event.which == 76){ //l
+            if(! $("#next").hasClass("disabled")){ startNext();}
+        }
+        
+        if(event.which == 82){ //r
+            reset();
+        }
+    });
     
     //handle dice roll
     $("#roll-it").click(function(){
-        rollHistoryIndex = 1;           //skip last element
-        disableButtons();
-        console.log(rollHistory);
-        rollIt(false);             //set click event handler, start preroll
+        startRoll();
     });
     
     //show previous roll
     $("#previous").click(function(){
-        clearDices();
-        enableNextButton();
-        rollHistoryIndex += 1;
-        highlightDice(rollHistory[rollHistory.length-(rollHistoryIndex)]);
-        if((rollHistory.length-1) < rollHistoryIndex+1){    //disable button if there are no more elements
-            disablePreviousButton();
-        }
-        console.log("Looking at: " + rollHistory[rollHistory.length-(rollHistoryIndex)]);
+        startPrevious();
+    });
+    
+    $("#next").click(function(){
+       startNext(); 
     });
    
 });
 
 function rollIt(donePreRolling){
     if(donePreRolling){
-        var randInt = 0;
+        var randInt = 0;        //closure
         
-        var ID2 = setInterval(function(){
+        var ID2 = setInterval(function(){   //...
             rolling_index += 1;
             var rolling_index_mod = rolling_index % 6;
             
@@ -57,6 +69,7 @@ function rollIt(donePreRolling){
             if(rolling_index > randInt){
                 rolling_index = 0;
                 rollHistory.push(randInt%6);  //add new roll to history
+                updateStatistics(randInt);
                 enableButtons();             //buttons can be used again
                 clearInterval(ID2);          //unregister interval loop
                 return;
@@ -76,7 +89,7 @@ function rollIt(donePreRolling){
 //Roll "amountOfLoops"-times before actually choosing a dice
 function preRoll(){
     clearDices();
-    var ID =  setInterval(function(){  //every 500ms...
+    var ID =  setInterval(function(){  //every 500ms... (this is an example of a closure btw)
             rolling_index += 1;
             var rolling_index_mod = rolling_index%6;   //allow multiple loops
             
@@ -90,7 +103,7 @@ function preRoll(){
                 console.log("changing dice" + rolling_index_mod);
                 if((rolling_index_mod) == 0){
                     selectDice(0);
-                    $("#dice0").css("color", "green");
+                    $("#dice0").css("color", "lime");
                     $("#dice5").css("color", "black");
                 }
                 else{
@@ -102,8 +115,38 @@ function preRoll(){
             }, preRollDuration/6);
 }
 
+function startRoll(){
+    rollHistoryIndex = 1;       //skip last element
+    if(rollHistory.length == 0){
+        initializeTable("0");
+    }
+    disableButtons();
+    rollIt(false);             //set click event handler, start preroll
+}
+
+function startPrevious(){
+    clearDices();
+    enableNextButton();
+    rollHistoryIndex += 1;
+    highlightDice(rollHistory[rollHistory.length-(rollHistoryIndex)]);
+    if((rollHistory.length-1) < rollHistoryIndex+1){    //disable button if there are no more elements
+        disablePreviousButton();
+    }
+    console.log("Looking at: " + rollHistory[rollHistory.length-(rollHistoryIndex)]);
+}
+
+function startNext(){
+    clearDices();
+    enablePreviousButton();
+    rollHistoryIndex -= 1;
+    highlightDice(rollHistory[rollHistory.length-(rollHistoryIndex)]);
+    if(rollHistoryIndex == 1){  //when arrived at last element
+        disableNextButton();
+    }
+}
+
 function selectDice(num){
-    $("#dice" + num).css("color", "green");
+    $("#dice" + num).css("color", "lime");
 }
 
 function highlightDice(num){
@@ -129,21 +172,22 @@ function disableButtons(){
 
 function enableButtons(){
     $("#roll-it").css("pointer-events","auto");    //make it clickable
-    $("#roll-it").removeClass("disabled");            //make it look active
+    $("#roll-it").removeClass("disabled");         //make it look active
     if(rollHistory.length > 1){
-    $("#previous").css("pointer-events","auto");
-    $("#previous").removeClass("disabled");
+        enablePreviousButton();
     }
 }
 
 function disablePreviousButton(){
     $("#previous").css("pointer-events","none");
     $("#previous").addClass("disabled");
+    console.log("deactivated previous");
 }
 
 function enablePreviousButton(){
     $("#previous").css("pointer-events","auto");
     $("#previous").removeClass("disabled");
+    console.log("activated previous");
 }
 
 function disableNextButton(){
@@ -154,6 +198,32 @@ function disableNextButton(){
 function enableNextButton(){
     $("#next").css("pointer-events","auto");
     $("#next").removeClass("disabled");
+}
+
+function initializeTable(initText){
+    for(var i=1; i<=6; i++){
+            $("#" + i + "-absolute").text(initText);
+            $("#" + i + "-relative").text(initText);
+    }
+}
+
+function updateStatistics(result){
+    var updateText = (parseInt($("#" + result + "-absolute").text())+1).toString();
+    $("#" + result + "-absolute").text(updateText);
+    
+    for(var i=1; i<=6; i++){
+        var absolute = parseInt($("#" + i + "-absolute").text());
+        var relative = 0;
+        var digits = 4;
+        relative = (absolute/rollHistory.length).toFixed(4);
+        $("#" + i + "-relative").text(parseFloat(relative));    //remove trailing zeros
+    }
+}
+
+function reset(){
+    rollHistory.length = 0;     //clear rollHistory
+    initializeTable("N. A.");
+    clearDices();
 }
     
     
